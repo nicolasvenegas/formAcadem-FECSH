19285f51247aaf2c33003c6221a7a5ff037fc05a
Nico <nicolas.venegas@gmail.com>
2026-08-31 16:37:05 -0400

    config: Update Apps Script URL to v4 deploy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FECSH_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw7e_cqvbjX_13Bce9UIWjgKU80yr2fuQ7zVA6UohYcbUY0Jh4472nV5VxWjO-JQFh3/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzihPz3be-LL7Cjf54Kq79ilThE_3P6sEPsjIlCG-jjxYRqXtjUUghh-CX3ZVJxnEIp/exec',
   AUTH_DOMAINS: ['@ufrontera.cl'],
   SHEET_ID: '1MGr4dJvx-tqzuYsfILi36R55JKeUPamTuICxVtA14v4'
 };
