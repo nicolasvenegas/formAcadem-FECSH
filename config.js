b9ce8f89b5a0ce87cdabce41ae0aa6a893e4dcaf
Nico <nicolas.venegas@gmail.com>
2026-08-31 16:52:42 -0400

    config: Use Cloudflare Worker proxy for CORS

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FECSH_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzihPz3be-LL7Cjf54Kq79ilThE_3P6sEPsjIlCG-jjxYRqXtjUUghh-CX3ZVJxnEIp/exec',
+  APPS_SCRIPT_URL: 'https://fecsh-proxy.nicolas-venegas.workers.dev/',
   AUTH_DOMAINS: ['@ufrontera.cl'],
   SHEET_ID: '1MGr4dJvx-tqzuYsfILi36R55JKeUPamTuICxVtA14v4'
 };
